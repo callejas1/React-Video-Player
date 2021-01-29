69cb9af34a6f9578cb8e7557809dcb4d87d9faa5
Yoselyn Callejas <yoselyncallejas19@gmail.com>
2021-01-29 13:04:45 +0100

    Included build folder

---
+++ build/precache-manifest.66daea74ee108bbed72f353dda1514e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6162c952f9b99173cd1d09d1981181b8",
+    "url": "/react_videoplayer/index.html"
+  },
+  {
+    "revision": "6806a41a4a4bdad1d7f9",
+    "url": "/react_videoplayer/static/js/2.9a5f570f.chunk.js"
+  },
+  {
+    "revision": "6f2b82ad1544a264ac58",
+    "url": "/react_videoplayer/static/js/main.663d3cc7.chunk.js"
+  },
+  {
+    "revision": "9d808bd6d5131ace177f",
+    "url": "/react_videoplayer/static/js/runtime~main.08c55151.js"
+  }
+]);
